cee2dc0c6f043f2acb4ee35c77e9835d2a6ef047
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-05-15 17:43:01 +0530

    add authentication and association

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,37 +1,27 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: %i[new create]
 
   def index
     @posts = Post.all
   end
-
 
   def new
     @post = Post.new
   end
 
   def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      flash.notice = "Post, '#{@post.title}' created!"
+      redirect_to root_path
+    else
+      render :new
     end
   end
 
-
   private
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    def post_params
-      params.require(:post).permit(:title, :description)
-    end
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
